535222ac7b3feab07d267eb2bc37013c424154d2
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-12 00:09:38 -0800

    Add save image function

--- utils.py
+++ utils.py
@@ -1,18 +1,31 @@
 from PIL import Image
 import numpy as np
 
+# Constant from the Gatys paper
 BGR_MEANS = np.array([103.939, 116.779, 123.68], dtype = np.float64)
 
-# Read and swap RGB to BGR as input
-
 def load_image(path):
-    im = Image.open(path)
-    im_data = np.array(im)
+    image = Image.open(path)
+    image_data = np.array(image)
     # if black and white images, add condition.
-    red = np.copy(im_data[:, :, :0])
-    blue = np.copy(im_data[:, :, :2])
-    im_data[:, :, 0] = blue
-    im_data[:, :, 2] = red
-    centered_im = im_data - BGR_MEANS
-    print (centered_im.shape)
-    return centered_im
+
+    # Read and swap RGB to BGR as input
+    red = np.copy(image_data[:, :, :0])
+    blue = np.copy(image_data[:, :, :2])
+    image_data[:, :, 0] = blue
+    image_data[:, :, 2] = red
+    centered_image = image_data - BGR_MEANS
+    print (centered_image.shape)
+    return centered_image
+
+def save_image(path, image_data):
+    image_data = image_date + BGR_MEANS
+    blue = np.copy(image_data[:, :, 0])
+    red = np.copy(image_data[:, :, 2])
+    image_data[:, :, 0] = red
+    image_data[:, :, 2] = blue
+    # correcting the range
+    image_data = np.clip(im_data, 0, 255)
+    image_data = image_data.astype(np.uint8)
+    image = Image.fromarray(image_data)
+    image.save(path)
