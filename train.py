b1f76fea61ea73c600191f3e681e74cafcdfdcab
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-03-11 16:36:54 -0700

    Fix feature tensor variable

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ reshaped_image = Reshape((768, 1024, 3))(image_tensor)
 content_tensor, *style_tensors = featurization_model(reshaped_image)
 feature_tensors = [content_tensor, *style_tensors]
 
-training_model = Model(inputs=input_tensor, outputs=feature_tensor)
+training_model = Model(inputs=input_tensor, outputs=feature_tensors)
 training_model.summary()
 
 def save_int_image(epoch_idx, logs):
